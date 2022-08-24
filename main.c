eddb3d3baf2d62ffb629407fa5f81e5fe0defa6b
홍은표 <44293278+potato179@users.noreply.github.com>
2022-08-24 12:19:33 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,13 +3,11 @@
 int table1[2][2][2];
 int table2[2][2][2];
 
-int at_(int a, int k)
-{
+int at_(int a, int k){
     return (a>>k) & 1;
 }
 
-int main()
-{
+int main(){
     table1[0][0][0] = 0;
     table1[1][0][0] = table1[0][1][0] = table1[0][0][1] = 1;
     table1[1][1][0] = table1[1][0][1] = table1[0][1][1] = 0;
@@ -25,8 +23,7 @@ int main()
     int c = 0;
 
     int carryin = 0;
-    for(int i=0; i<=31; i++)
-    {
+    for(int i=0; i<=31; i++){
         int a_ = at_(a, i), b_ = at_(b, i);
 
         int c_ = table1[a_][b_][carryin];
